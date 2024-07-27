fe30458b1d6aafdec51bf652602d992fccd08580
arjo31 <arjo3103@gmail.com>
2024-07-28 00:12:14 +0530

    Data Table added in accounts

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,13 +1,36 @@
-'use client';
+"use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
+import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
 
 function AccountsPage() {
+  const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
 
-    const newAccount = useNewAccount();
+    if (accountsQuery.isLoading) {
+        return (
+            <div className="max-w-screen-2xl mx-auto -mt-24 w-full pb-10">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader>
+                        <Skeleton className="h-8 w-48"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin"/>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+
   return (
     <div className="max-w-screen-2xl mx-auto -mt-24 w-full pb-10">
       <Card className="border-none drop-shadow-sm">
@@ -15,9 +38,18 @@ function AccountsPage() {
           <CardTitle className="text-xl line-clamp-1">Accounts Page</CardTitle>
           <Button onClick={newAccount.onOpen} size="sm">
             <Plus className="size-4 mr-2"></Plus>
-            Add New
+            Add New Account
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable
+            filterKey="name"
+            columns={columns}
+            data={accounts}
+            onDelete={() => {}}
+            disabled={false}
+          />
+        </CardContent>
       </Card>
     </div>
   );
