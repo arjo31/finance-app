1fbe6d34f3b520fef8a8d0b2ab932c0fb0edd965
arjo31 <arjo3103@gmail.com>
2024-07-29 00:46:24 +0530

    Added categories schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
-import {createInsertSchema} from "drizzle-zod";
+import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -10,3 +10,10 @@ export const accounts = pgTable("accounts", {
 
 //Schema for inserting a user - can be used to validate API requests
 export const insertAccountSchema = createInsertSchema(accounts);
+
+export const categories = pgTable("categories", {
+  id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
+});
